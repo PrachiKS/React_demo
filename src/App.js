4b1b8067ee6909ad187cc7c66e38e38f602e1dc5
PrachiKS <prachiksonawane25@gmail.com>
2026-02-19 22:14:18 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,24 +5,60 @@ import './App.css';
 //import Component3 from './Components/Component3';
 import Login from './Components/Login';
 import Notlogin from './Components/Notlogin';
-import { useEffect, useState } from 'react';
+import { useEffect, useReducer, useState } from 'react';
 
 
 function App() {
 
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
   //let isVisible = false;
   const [loggedIn, setloggedIn] = useState(false)
 
-  const handleClick = () => {
-    setCount(count+1)
+  // const handleIncrement = () => {
+  //   setCount(count+1)
+    
+  // }
+
+  // const handleDecrement = () => {
+  //   setCount(count-1)
+  // }
+
+  const initialState = {
+    count:0,
+    toggle:false,
+    data:""
+  }
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+    return {count: state.count+1};
+    case "DECREMENT":
+      return {count:state.count - 1};
+    case "TOGGLE":
+      return {toggle:!state.toggle};
+    case "DATA":
+      return{data:state.data="Test"};
+    default:
+      return state
+  }
+}
+
+  const[state, dispatch]=useReducer(reducer, initialState)
+  const handleIncrement = ()=> {
+    dispatch({ type: "INCREMENT"});
     
   }
+  const handleDecrement = ()=> {
+    dispatch({type: "DECREMENT"});
+  }
 
-  useEffect(() => {
-    alert("Your count is increased")
-  }, []);
+  // useEffect(() => {
+  //   alert("Your count is increased")
+  // }, []);
   // let loggedIn =true;
+
+  console.log(state.count)
+  console.log(state.toggle)
   return (
     <>
       {/* <>
@@ -32,9 +68,14 @@ function App() {
       {loggedIn ? <Login/> : <Notlogin loggedIn={loggedIn} setloggedIn={setloggedIn}/>}
 
         <>
-        <h1>{count}</h1>
-          <button onClick={handleClick}>Count</button>
-        </>
+        <h1>{state.count}</h1>
+          {/* <button onClick={handleIncrement}>+</button> */}
+          <button onClick={handleDecrement}>-</button>
+          <button onClick={handleIncrement}>+</button>
+          <button onClick={() => dispatch({type: "TOGGLE"})}>Toggle</button>
+          <h1>{state.data}</h1>
+          <button onClick={() => dispatch({type: "DATA"})}>Get DATA</button>
+        </> 
     </>
   );
 }
